903f4d7bac3e98d15b092dca27eeb944afc684d5
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-12-20 18:49:34 +0600

    functionalities started

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        return render_template('index.html',)
+    return render_template('login.html')
+
 
 @app.route('/')
 def index():
@@ -22,6 +35,10 @@ def campaigns():
 @app.route('/smart_inbox')
 def smart_inbox():
     return render_template('smart_inbox.html')
+
+
+   
+
 
 if __name__ == '__main__':
     app.run(debug=True)
